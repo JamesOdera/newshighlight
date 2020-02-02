c37fcd60218554fb01b33abec72d3ee08befbb62
JamesOdera <nyangoma.odera@gmail.com>
2020-02-02 22:33:16 +0300

    add changes to url

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     NEWS_API_BASE_URL ='https://newsapi.org/v2/sources?apiKey=c7e4aad3be1347a9aab0ecd2b4c850f5'
-    ARTICLES_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=c7e4aad3be1347a9aab0ecd2b4c850f5'
+    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey=c7e4aad3be1347a9aab0ecd2b4c850f5'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
